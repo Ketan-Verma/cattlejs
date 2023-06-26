dd3b7a3c093194933dc9d8e09c088ab4bc43ac16
Ketan-Verma <ketanjverma@gmail.com>
2023-06-26 21:02:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+// Load the JSON data
+const dataURL = "./data.json";
+
+// Global variable to store the loaded data
+let Cdata = [];
+
+// Function to fetch the JSON data
+function loadData() {
+  fetch(dataURL)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      Cdata = jsonData;
+      setupAutocomplete();
+    })
+    .catch((error) => {
+      console.error("Error fetching JSON data:", error);
+    });
+}
+
+// Function to setup the autocomplete input
+function setupAutocomplete() {
+  const searchInput = document.getElementById("searchInput");
+
+  searchInput.addEventListener("input", handleSearchInput);
+}
+
+// Function to handle search input changes
+function handleSearchInput(event) {
+  const inputValue = event.target.value.trim().toUpperCase();
+  const filteredData = Cdata.filter((item) => item.Sno.includes(inputValue));
+  displayData(filteredData);
+}
+
+// Function to display the selected data
+function displayData(data) {
+  const dataContainer = document.getElementById("dataContainer");
+
+  // Clear previous data
+  dataContainer.innerHTML = "";
+
+  // Create and append the data elements
+  data.forEach((item) => {
+    const div = document.createElement("div");
+    /*<table class="table">
+  <thead>
+  
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>*/
+    div.innerHTML = `
+    <table class="table">
+    <tbody>
+    <tr>
+      <th scope="row">Serial Number:</th>
+      <td>${item.Sno}</td>
+    </tr>
+    
+    <tr>
+      <th scope="row">Date of Birth: </th>
+      <td>${item.DOB}</td>
+    </tr>
+    
+    <tr>
+      <th scope="row">Date of Calving:</th>
+      <td>${item.DOC}</td>
+    </tr>
+    
+    <tr>
+      <th scope="row">Parity:</th>
+      <td>${item.Parity}</td>
+    </tr>
+    
+    <tr>
+      <th scope="row">Sex:</th>
+      <td> ${item.Sex}</td>
+    </tr>
+    </tbody>
+    </table>
+                     `;
+    dataContainer.appendChild(div);
+  });
+}
+
+// Load the data and set up the autocomplete
+loadData();
